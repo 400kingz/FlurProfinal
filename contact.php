00a4d318a01b19a0fb92579585614c597bbfdeec
400kingz <flurpro@gmail.com>
2023-02-01 20:20:01 -0800

    1.0.1

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // collect form data
+    $name = $_POST['cf_name'];
+    $email = $_POST['cf_email'];
+    $message = $_POST['cf_message'];
+
+    // validate form data
+    if (empty($name) || empty($email) || empty($message)) {
+        echo 'Please fill out all fields';
+        exit;
+    }
+
+    // send the email
+    $to = 'you@example.com';
+    $subject = 'New Contact Form Submission';
+    $body = "From: $name\nEmail: $email\nMessage:\n$message";
+    mail($to, $subject, $body);
+
+    echo 'Thank you for your message';
+    exit;
+}
+
+?>
